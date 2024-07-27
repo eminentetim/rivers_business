f2cf5b95cca8142f95330207864515d121e76ac5
eminentetim <etimeminent@gmail.com>
2024-07-27 07:43:43 +0100

    API

--- app.js
+++ app.js
@@ -43,13 +43,13 @@ app.use(session({
 app.set('view engine', 'ejs');
 
 // Routes
-app.use('/auth', require('./routes/auth'));
-app.use('/', require('./routes/index'));
-app.use('/admin', require('./routes/admin'));
-app.use('/student', require('./routes/student'));
-app.use('/application', require('./routes/application'));
-app.use('/payment', require('./routes/payment'));
-app.use('/notifications', require('./routes/notification'));
-
+// Using routes
+app.use('/api/auth', authRoutes);
+app.use('/api', indexRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/student', studentRoutes);
+app.use('/api/application', applicationRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use('/api/notifications', notificationRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
